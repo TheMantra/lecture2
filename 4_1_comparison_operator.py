4be278b22faefa5d34a6608320daad82324bc49a
TheMantra <36901110+TheMantra@users.noreply.github.com>
2018-03-06 23:51:27 +0330

    Update 4_1_comparison_operator.py
    changes the variables & their values & omit one line of the program

--- 4_1_comparison_operator.py
+++ 4_1_comparison_operator.py
@@ -1,11 +1,11 @@
 ##########################
 ## compare two variables #
 ##########################
-i=5
-j=6
-print("i < j --> {0}".format(i<j))
-print("i > j --> {0}".format(i>j))
-print("i <= j --> {0}".format(i<=j))
-print("i >= j --> {0}".format(i>=j))
-print("i is equal to j --> {0}".format(i==j))
-print("i is not equal to j --> {0}".format(i!=j))
+a = 8
+b = 6
+print("a < b --> {0}".format(i<j))
+print("a > b --> {0}".format(i>j))
+#print("a <= b --> {0}".format(i<=j))
+print("a >= b --> {0}".format(i>=j))
+print("a is equal to b --> {0}".format(i==j))
+print("a is not equal to b --> {0}".format(i!=j))
